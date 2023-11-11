f7f3534ed9a7563cd184fc3c5061640793758091
LittleCrowRevi <snowrevy@gmail.com>
2023-11-11 15:59:55 +0100

    add terminal crate--

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -1,17 +1,19 @@
 use bevy::app::{App, Plugin, Startup, Update};
-use bevy::DefaultPlugins;
 use bevy::input::Input;
-use bevy::math::{Vec2, Vec3};
-use bevy::prelude::{Camera2dBundle, Color, Commands, Component, KeyCode, Mut, Projection, Query, Res, ResMut, Resource, TextBundle, Time, Transform};
+use bevy::math::{Rect, Vec2, Vec3};
+use bevy::prelude::{
+    Camera2dBundle, Color, Commands, Component, KeyCode, Mut, Projection, Query, Res, ResMut,
+    Resource, Sprite, TextBundle, Time, Transform,
+};
+use bevy::sprite::SpriteBundle;
 use bevy::text::{Text, TextAlignment, TextSection, TextStyle};
 use bevy::time::{Timer, TimerMode};
 use bevy::ui::{PositionType, Style};
 use bevy::utils::default;
+use bevy::DefaultPlugins;
 
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, NebulaVault))
-        .run();
+    App::new().add_plugins((DefaultPlugins, NebulaVault)).run();
 }
 
 #[derive(Component)]
@@ -28,24 +30,54 @@ struct NebulaTime(Timer);
 #[derive(Component)]
 struct TagCamera;
 
+
 const SCORE_COLOR: Color = Color::rgb(0.6, 0.6, 0.8);
 
 fn setup(mut commnads: Commands) {
     println!("Starting NebulaVault!");
 
-    commnads.spawn((Camera2dBundle { transform: Transform::default(), ..default()}, TagCamera));
+    commnads.spawn((
+        Camera2dBundle {
+            transform: Transform::default(),
+            ..default()
+        },
+        TagCamera,
+    ));
 
     commnads.spawn(
         TextBundle::from_section(
             "Count: 0",
-            TextStyle { font_size: 30f32, color: SCORE_COLOR, ..default() },
+            TextStyle {
+                font_size: 30f32,
+                color: SCORE_COLOR,
+                ..default()
+            },
         )
-            .with_text_alignment(TextAlignment::Center)
-            .with_style(Style { position_type: PositionType::Absolute, ..default() })
+        .with_text_alignment(TextAlignment::Center)
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            ..default()
+        }),
     );
+
+    commnads.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.55),
+            custom_size: Some(Vec2::new(50.0, 100.0)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(-50.0, 0.0, 0.0)),
+        ..default()
+    });
 }
 
-fn print_timer(time: Res<Time>, mut timer: ResMut<NebulaTime>, query: Query<&Item>, mut commands: Commands, mut count_text: Query<&mut Text>) {
+fn print_timer(
+    time: Res<Time>,
+    mut timer: ResMut<NebulaTime>,
+    query: Query<&Item>,
+    mut commands: Commands,
+    mut count_text: Query<&mut Text>,
+) {
     if timer.0.tick(time.delta()).just_finished() {
         let mut count = query.iter().count();
         count += 1;
@@ -54,7 +86,9 @@ fn print_timer(time: Res<Time>, mut timer: ResMut<NebulaTime>, query: Query<&Ite
         text.sections[0].value = format!("Count: {count}");
         println!("Tick! Items: {count}");
 
-        commands.spawn(Item { number: (count + 1) as i64 });
+        commands.spawn(Item {
+            number: (count + 1) as i64,
+        });
     };
 }
 
@@ -62,7 +96,7 @@ fn handle_input(keys: Res<Input<KeyCode>>, mut query: Query<(&mut TagCamera, &mu
     for (mut camera, mut transform) in query.iter_mut() {
         let mut pan = Vec2::ZERO;
         let mut distance = Vec3::ZERO;
-        
+
         // if let Projection::Perspective(projection) = projection {
         //     pan *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov);
         // }
@@ -70,10 +104,9 @@ fn handle_input(keys: Res<Input<KeyCode>>, mut query: Query<(&mut TagCamera, &mu
         if keys.just_pressed(KeyCode::Left) {}
 
         if keys.pressed(KeyCode::Right) {
-            distance = Vec3::X;
-            println!("{:#?}", distance);
+            distance = Vec3::new(5.0, 0.0, 0.0);
+            transform.translation += distance;
         }
-        transform.translation = distance;
     }
 }
 
@@ -81,8 +114,7 @@ struct NebulaVault;
 
 impl Plugin for NebulaVault {
     fn build(&self, app: &mut App) {
-        app
-            .insert_resource(NebulaTime(Timer::from_seconds(1.0, TimerMode::Repeating)))
+        app.insert_resource(NebulaTime(Timer::from_seconds(1.0, TimerMode::Repeating)))
             .add_systems(Update, (print_timer, handle_input))
             .add_systems(Startup, setup);
     }
