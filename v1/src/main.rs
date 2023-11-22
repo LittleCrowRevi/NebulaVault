edc257e2b3e5358b1d35441119c47a70404cb847
LittleCrowRevi <snowrevy@gmail.com>
2023-11-22 21:05:13 +0100

    create bundle, add to player

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -76,6 +76,12 @@ struct CoreStats {
     wisdom: i32,
 }
 
+#[derive(Bundle)]
+struct EntityBundle {
+    core_stats: CoreStats,
+    vital_stats: VitalStats
+}
+
 // Grid
 #[derive(Component)]
 struct ActiveGrid;
@@ -150,6 +156,16 @@ fn setup(mut commands: Commands) {
         Player,
         Movement(0f32, false),
         GridPos(vec3(-1.0, -1.0, 1.0)),
+        EntityBundle {
+            vital_stats: VitalStats {
+                energy: 100,
+                health: 100,
+                mana: 100
+            },
+            core_stats: CoreStats {
+                agility: 10, constitution: 10, fortune: 10, intelligence: 10, strength: 10, wisdom: 10
+            }
+        }
     )).with_children(|parent| {
         parent.spawn(
             Text2dBundle {
